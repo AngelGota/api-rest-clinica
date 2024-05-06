182beafd28466571345358e3e744665e9d2ff904
Angelo <gangelos630@gmail.com>
2024-05-05 19:31:32 -0500

    api funcional

---
+++ index.js
@@ -0,0 +1,85 @@
+import express from "express";
+import fs, { read } from "fs";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const readData = () => {
+  try {
+    const data = fs.readFileSync("./db.json");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const writeData = (data) => {
+  try {
+    fs.writeFileSync("./db.json", JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+readData();
+
+app.get("/", (req, res) => {
+  res.send("Api Rest for clinical appointment development by Angelo Gomez :D");
+});
+
+app.get("/medicalAppointments", (req, res) => {
+  const data = readData();
+  res.json(data.medicalAppointments);
+});
+
+app.get("/medicalAppointments/:id", (req, res) => {
+  const data = readData();
+  const id = parseInt(req.params.id);
+  const appointment = data.medicalAppointments.find(
+    (appointment) => appointment.id === id
+  );
+  res.json(appointment);
+});
+
+app.post("/medicalAppointments", (req, res) => {
+  const data = readData();
+  const body = req.body;
+  const newAppointment = {
+    id: data.medicalAppointments.length + 1,
+    ...body,
+  };
+  data.medicalAppointments.push(newAppointment);
+  writeData(data);
+  res.json(newAppointment);
+});
+
+app.put("/medicalAppointments/:id", (req, res) => {
+  const data = readData();
+  const body = req.body;
+  const id = parseInt(req.params.id);
+  const appointmentIndex = data.medicalAppointments.findIndex(
+    (appointment) => appointment.id === id
+  );
+  data.medicalAppointments[appointmentIndex] = {
+    ...data.medicalAppointments[appointmentIndex],
+    ...body,
+  };
+  writeData(data);
+  res.json({ message: "Medical appointment updated successfully" });
+});
+
+app.delete("/medicalAppointments/:id", (req, res) => {
+  const data = readData();
+  const id = parseInt(req.params.id);
+  const appointmentIndex = data.medicalAppointments.findIndex(
+    (appointment) => appointment.id === id
+  );
+  data.medicalAppointments.splice(appointmentIndex, 1);
+  writeData(data);
+  res.json({ message: "Medical appointment deleted successfully" });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
